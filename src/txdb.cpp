d5240547eb671503e578ac6df92def30d1acb176
BtcDrak <btcdrak@gmail.com>
2014-07-01 16:41:58 +0100

    Fix merge error.

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -213,8 +213,8 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
 
-                if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits))
-                    return error("LoadBlockIndex() : CheckProofOfWork failed: %s", pindexNew->ToString());
+                //if (!CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits))
+                //    return error("LoadBlockIndex() : CheckProofOfWork failed: %s", pindexNew->ToString());
 
                 pcursor->Next();
             } else {
