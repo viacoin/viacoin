7eadfe1e8b58b2f0a76d0a558bc718e8bbac13b2
Grynn San <grynnsan#gmail.com>
2014-07-29 07:30:25 -0700

    include PoW in getblock output

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -69,6 +69,7 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex)
     result.push_back(Pair("bits", strprintf("%08x", block.nBits)));
     result.push_back(Pair("difficulty", GetDifficulty(blockindex)));
     result.push_back(Pair("chainwork", blockindex->nChainWork.GetHex()));
+    result.push_back(Pair("PoW", block.GetPoWHash().GetHex()));
 
     if (blockindex->pprev)
         result.push_back(Pair("previousblockhash", blockindex->pprev->GetBlockHash().GetHex()));
