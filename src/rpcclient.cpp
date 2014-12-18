85ea16a1651b41b0744897c7cea1ae3493595565
reorder <reorder@users.noreply.github.com>
2014-12-18 11:59:30 +0300

    Fix typo in searchrawtransactions RPC call name

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -73,7 +73,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "createrawtransaction", 1 },
     { "signrawtransaction", 1 },
     { "signrawtransaction", 2 },
-    { "searchrawtransaction", 2 },
+    { "searchrawtransactions", 2 },
     { "sendrawtransaction", 1 },
     { "gettxout", 1 },
     { "gettxout", 2 },
