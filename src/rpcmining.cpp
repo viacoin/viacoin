8b11cb735852c4bf8d1557f5189798f9e12eab1b
reorder <reorder@users.noreply.github.com>
2014-12-21 22:08:47 +0300

    Bail out from getauxblock before hardfork height

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -697,6 +697,8 @@ Value getauxblock(const Array& params, bool fHelp)
         static uint64_t nStart;
         static CBlock* pblock;
         static CBlockTemplate* pblocktemplate;
+        if (pindexPrev->nHeight < GetAuxPowStartBlock() - 1)
+            throw JSONRPCError(-1, "Merged mining not enabled at current block height yet");
         if (pindexPrev != chainActive.Tip() ||
             (mempool.GetTransactionsUpdated() != nTransactionsUpdatedLast && GetTime() - nStart > 20))
         {
