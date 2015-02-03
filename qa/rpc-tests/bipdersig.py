39182ba8561ffdafbcd52abae1a259d8b8b79e10
BtcDrak <btcdrak@gmail.com>
2015-02-03 20:15:20 +0000

    Change block version for tests

--- qa/rpc-tests/bipdersig.py
+++ qa/rpc-tests/bipdersig.py
@@ -18,8 +18,8 @@ class BIP66Test(BitcoinTestFramework):
     def setup_network(self):
         self.nodes = []
         self.nodes.append(start_node(0, self.options.tmpdir, []))
-        self.nodes.append(start_node(1, self.options.tmpdir, ["-blockversion=2"]))
-        self.nodes.append(start_node(2, self.options.tmpdir, ["-blockversion=3"]))
+        self.nodes.append(start_node(1, self.options.tmpdir, ["-blockversion=3"]))
+        self.nodes.append(start_node(2, self.options.tmpdir, ["-blockversion=4"]))
         connect_nodes(self.nodes[1], 0)
         connect_nodes(self.nodes[2], 0)
         self.is_network_split = False
@@ -32,14 +32,14 @@ class BIP66Test(BitcoinTestFramework):
         self.nodes[1].setgenerate(True, 100)
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 100):
-            raise AssertionError("Failed to mine 100 version=2 blocks")
+            raise AssertionError("Failed to mine 100 version=3 blocks")
 
         # Mine 750 new-version blocks
         for i in xrange(15):
             self.nodes[2].setgenerate(True, 50)
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 850):
-            raise AssertionError("Failed to mine 750 version=3 blocks")
+            raise AssertionError("Failed to mine 750 version=4 blocks")
 
         # TODO: check that new DERSIG rules are not enforced
 
@@ -47,7 +47,7 @@ class BIP66Test(BitcoinTestFramework):
         self.nodes[2].setgenerate(True, 1)
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 851):
-            raise AssertionFailure("Failed to mine a version=3 blocks")
+            raise AssertionFailure("Failed to mine a version=4 blocks")
 
         # TODO: check that new DERSIG rules are enforced
 
@@ -56,35 +56,35 @@ class BIP66Test(BitcoinTestFramework):
             self.nodes[2].setgenerate(True, 99)
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 1049):
-            raise AssertionError("Failed to mine 198 version=3 blocks")
+            raise AssertionError("Failed to mine 198 version=4 blocks")
 
         # Mine 1 old-version block
         self.nodes[1].setgenerate(True, 1)
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 1050):
-            raise AssertionError("Failed to mine a version=2 block after 949 version=3 blocks")
+            raise AssertionError("Failed to mine a version=3 block after 949 version=3 blocks")
 
         # Mine 1 new-version blocks
         self.nodes[2].setgenerate(True, 1)
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 1051):
-            raise AssertionError("Failed to mine a version=3 block")
+            raise AssertionError("Failed to mine a version=4 block")
 
         # Mine 1 old-version blocks
         try:
             self.nodes[1].setgenerate(True, 1)
-            raise AssertionError("Succeeded to mine a version=2 block after 950 version=3 blocks")
+            raise AssertionError("Succeeded to mine a version=3 block after 950 version=3 blocks")
         except JSONRPCException:
             pass
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 1051):
-            raise AssertionError("Accepted a version=2 block after 950 version=3 blocks")
+            raise AssertionError("Accepted a version=3 block after 950 version=3 blocks")
 
         # Mine 1 new-version blocks
         self.nodes[2].setgenerate(True, 1)
         self.sync_all()
         if (self.nodes[0].getblockcount() != cnt + 1052):
-            raise AssertionError("Failed to mine a version=3 block")
+            raise AssertionError("Failed to mine a version=4 block")
 
 if __name__ == '__main__':
     BIP66Test().main()
