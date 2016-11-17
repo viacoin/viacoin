5723c8dc5ee523e11d5b93da1e05d9761c6bbd23
romanornr <romanornr@gmail.com>
2016-11-17 22:32:24 +0100

    scratchpad alignment fix scrypt.h

--- src/crypto/scrypt.h
+++ src/crypto/scrypt.h
@@ -11,19 +11,16 @@ void scrypt_1024_1_1_256_sp_generic(const char *input, char *output, char *scrat
 #if defined(USE_SSE2)
 #if defined(_M_X64) || defined(__x86_64__) || defined(_M_AMD64) || (defined(MAC_OSX) && defined(__i386__))
 #define USE_SSE2_ALWAYS 1
-#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_sse2((input), (output), 
-(scratchpad))
+#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_sse2((input), (output), (scratchpad))
 #else
-#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_detected((input), (output), 
-(scratchpad))
+#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_detected((input), (output), (scratchpad))
 #endif
 
 void scrypt_detect_sse2();
 void scrypt_1024_1_1_256_sp_sse2(const char *input, char *output, char *scratchpad);
 extern void (*scrypt_1024_1_1_256_sp_detected)(const char *input, char *output, char *scratchpad);
 #else
-#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_generic((input), (output), 
-(scratchpad))
+#define scrypt_1024_1_1_256_sp(input, output, scratchpad) scrypt_1024_1_1_256_sp_generic((input), (output), (scratchpad))
 #endif
 
 void
