58f4e3f7b32b067e13007299d4687582553da05b
romanornr <romanornr@gmail.com>
2016-11-19 21:42:52 +0100

    bloom test strSecret

--- src/test/bloom_tests.cpp
+++ src/test/bloom_tests.cpp
@@ -86,7 +86,7 @@ BOOST_AUTO_TEST_CASE(bloom_create_insert_serialize_with_tweak)
 
 BOOST_AUTO_TEST_CASE(bloom_create_insert_key)
 {
-    string strSecret = string("5Kg1gnAjaLfKiwhhPpGS3QfRg2m6awQvaj98JCZBZQ5SuS2F15C");
+    string strSecret = string("7hgsDF315YgQJBxmkyLNmBEQh8V5jwF6T7AmcqPq8vYywZn4gKi");
     CBitcoinSecret vchSecret;
     BOOST_CHECK(vchSecret.SetString(strSecret));
 
