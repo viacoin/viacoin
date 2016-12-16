a2302128420eec6a3fef4e08487b70cf93fde8d5
romanornr <romanornr@gmail.com>
2016-12-16 17:24:24 +0100

    Viacoin: [qa] fix rpc tests mempool&getchaintips

--- qa/rpc-tests/mempool_spendcoinbase.py
+++ qa/rpc-tests/mempool_spendcoinbase.py
@@ -33,13 +33,13 @@ class MempoolSpendCoinbaseTest(BitcoinTestFramework):
 
     def run_test(self):
         chain_height = self.nodes[0].getblockcount()
-        assert_equal(chain_height, 200)
+        assert_equal(chain_height, 7200)
         node0_address = self.nodes[0].getnewaddress()
 
         # Coinbase at height chain_height-100+1 ok in mempool, should
         # get mined. Coinbase at height chain_height-100+2 is
         # is too immature to spend.
-        b = [ self.nodes[0].getblockhash(n) for n in range(101, 103) ]
+        b = [ self.nodes[0].getblockhash(n) for n in range(3601, 3603) ]
         coinbase_txids = [ self.nodes[0].getblock(h)['tx'][0] for h in b ]
         spends_raw = [ create_tx(self.nodes[0], txid, node0_address, 49.99) for txid in coinbase_txids ]
 
