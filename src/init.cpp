938983e9fae604b4e0e7b50bdd5f359f61409861
romanornr <romanornr@gmail.com>
2017-01-03 11:58:40 +0100

    Viacoin: Copyright 2014

--- src/init.cpp
+++ src/init.cpp
@@ -483,7 +483,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/viacoin/viacoin>";
     const std::string URL_WEBSITE = "<https://viacore.org>";
     // todo: remove urls from translations on next change
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2014, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
