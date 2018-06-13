c18b38e22efa7784303709317519c5effb4f66a1
Romano <romanornr@gmail.com>
2018-06-14 00:32:15 +0200

    Viacoin: Add scrypt PoW

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -5,14 +5,22 @@
 
 #include <primitives/block.h>
 
-#include <hash.h>
-#include <tinyformat.h>
-#include <utilstrencodings.h>
-#include <crypto/common.h>
+#include "hash.h"
+#include "tinyformat.h"
+#include "utilstrencodings.h"
+#include "crypto/common.h"
+#include "crypto/scrypt.h"
 
 uint256 CBlockHeader::GetHash() const
 {
     return SerializeHash(*this);
+}
+
+uint256 CBlockHeader::GetPoWHash() const
+{
+    uint256 thash;
+    scrypt_1024_1_1_256(BEGIN(nVersion), BEGIN(thash));
+    return thash;
 }
 
 std::string CBlock::ToString() const
