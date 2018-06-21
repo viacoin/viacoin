1424024890029f783f6a3d1e826279741129eb47
romanornr <romanornr@gmail.com>
2018-06-21 22:22:04 +0200

    Viacoin: Dark Gravity Wave V3

--- src/pow.h
+++ src/pow.h
@@ -20,4 +20,5 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&);
 
+
 #endif // BITCOIN_POW_H
