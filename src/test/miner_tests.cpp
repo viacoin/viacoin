c0c2151bd78e913859e765cbbc78398dfb831b4b
romanornr <romanornr@gmail.com>
2019-05-07 23:20:37 +0200

    Viacoin: [test] createNewBlock validity amount

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -804,7 +804,7 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     }
     BOOST_CHECK(pblocktemplate = AssemblerForTest(chainparams).CreateNewBlock(scriptPubKey));
     // Extend to a 210000-long block chain.
-    while (chainActive.Tip()->nHeight < 210000) {
+    while (chainActive.Tip()->nHeight < 657000) {
         CBlockIndex* prev = chainActive.Tip();
         CBlockIndex* next = new CBlockIndex();
         next->phashBlock = new uint256(InsecureRand256());
