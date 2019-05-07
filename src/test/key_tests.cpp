36d8e29eb91e571708751cf4536cca8f96c40568
romanornr <romanornr@gmail.com>
2019-05-07 14:29:08 +0200

    Viacoin: [test] key tests

--- src/test/key_tests.cpp
+++ src/test/key_tests.cpp
@@ -16,14 +16,14 @@
 
 #include <boost/test/unit_test.hpp>
 
-static const std::string strSecret1 = "5HxWvvfubhXpYYpS3tJkw6fq9jE9j18THftkZjHHfmFiWtmAbrj";
-static const std::string strSecret2 = "5KC4ejrDjv152FGwP386VD1i2NYc5KkfSMyv1nGy1VGDxGHqVY3";
-static const std::string strSecret1C = "Kwr371tjA9u2rFSMZjTNun2PXXP3WPZu2afRHTcta6KxEUdm1vEw";
-static const std::string strSecret2C = "L3Hq7a8FEQwJkW1M2GNKDW28546Vp5miewcCzSqUD9kCAXrJdS3g";
-static const std::string addr1 = "1QFqqMUD55ZV3PJEJZtaKCsQmjLT6JkjvJ";
-static const std::string addr2 = "1F5y5E5FMc5YzdJtB9hLaUe43GDxEKXENJ";
-static const std::string addr1C = "1NoJrossxPBKfCHuJXT4HadJrXRE9Fxiqs";
-static const std::string addr2C = "1CRj2HyM1CXWzHAXLQtiGLyggNT9WQqsDs";
+static const std::string strSecret1 = "7fyNTPYB6uYu7o5WR3NhesEpApx8szxdA3vPtN7wFHjFZ2Ym8U5";
+static const std::string strSecret2 = "7hCvBCiVF829bVY1kCC3Cyah3UGbEKaqJk1ZLR7cb1jkzQXUVCD";
+static const std::string strSecret1C = "WT1ohUUbb4wo2zNtq7UnS8jyCNeMn6pWawtg1iDcS4qVDyoQYAuv";
+static const std::string strSecret2C = "WYTbi2i7fKz4wEwtHePijrjhjuMp5o2LDJqTihSC58FjA32WjpXd";
+static const std::string addr1 = "VyFfk4dfUtTh7ACP3MYCk7CGTYdRZqPK11";
+static const std::string addr2 = "Vp5nywEhmQym4QD2uwLy1Nxuj5Wvfae8kF";
+static const std::string addr1C = "Vwo8mX3LNC5XiyC43K6giUxAYLiCdRTSRo";
+static const std::string addr2C = "VmRYw18oR1Rj444g5CYLhFJYNBk7zRgRDr";
 
 static const std::string strAddressBad = "1HV9Lc3sNHZxwj4Zk6fB38tEmBryq2cBiF";
 
