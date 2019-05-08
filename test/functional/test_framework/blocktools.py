896f94f8581e0d661b084211d15bf2497d4c3f62
romanornr <romanornr@gmail.com>
2019-05-09 00:33:08 +0200

    Viacoin: [test] blocktools create coinbase

--- test/functional/test_framework/blocktools.py
+++ test/functional/test_framework/blocktools.py
@@ -110,9 +110,11 @@ def create_coinbase(height, pubkey=None):
 
     If pubkey is passed in, the coinbase output will be a P2PK output;
     otherwise an anyone-can-spend output."""
+    def ser_cb_height(height):
+        return bytes([0x50 + height] * 2) if (1 <= height <= 16) else ser_string(serialize_script_num(height))
     coinbase = CTransaction()
     coinbase.vin.append(CTxIn(COutPoint(0, 0xffffffff),
-                        ser_string(serialize_script_num(height)), 0xffffffff))
+                        ser_cb_height(height), 0xffffffff))
     coinbaseoutput = CTxOut()
     coinbaseoutput.nValue = 50 * COIN
     halvings = int(height / 150)  # regtest
