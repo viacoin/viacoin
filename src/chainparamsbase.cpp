23ace0a9022f37e548090fde07033dfb11b5cc8b
romanornr <romanornr@gmail.com>
2023-01-19 01:28:07 +0100

    Viacoin: chainparamsbase port numbers

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -43,13 +43,13 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN) {
-        return std::make_unique<CBaseChainParams>("", 8332, 8334);
+        return std::make_unique<CBaseChainParams>("", 5222, 5224);
     } else if (chain == CBaseChainParams::TESTNET) {
-        return std::make_unique<CBaseChainParams>("testnet3", 18332, 18334);
+      return std::make_unique<CBaseChainParams>("testnet3", 25222, 25224);
     } else if (chain == CBaseChainParams::SIGNET) {
-        return std::make_unique<CBaseChainParams>("signet", 38332, 38334);
+      return std::make_unique<CBaseChainParams>("signet", 35222, 35224);
     } else if (chain == CBaseChainParams::REGTEST) {
-        return std::make_unique<CBaseChainParams>("regtest", 18443, 18445);
+      return std::make_unique<CBaseChainParams>("regtest", 15443, 15445);
     }
     throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
