77d240ec5c902671c8eb43ff96649bd1c8ae299f
Vincent Swarte <vincentswarte@gmail.com>
2023-01-27 21:39:56 +0100

    Stop viacoin from hijacking my bitcoin datadir

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -861,7 +861,7 @@ fs::path GetDefaultDataDir()
     // Unix-like: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Viacoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -871,10 +871,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // macOS
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Viacoin";
 #else
     // Unix-like
-    return pathRet / ".bitcoin";
+    return pathRet / ".viacoin";
 #endif
 #endif
 }
