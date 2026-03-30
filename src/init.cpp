e0f68e7cbcf3ca7be7ab62f278145010bb0fc98d
romanornr <6548898+romanornr@users.noreply.github.com>
2026-03-31 00:31:06 +0200

    viacoin: use viacoin.conf and viacoind paths in defaults and init docs
    - change default config filename to viacoin.conf
    - change default pid filename to viacoind.pid
    - update default data/config paths to Viacoin locations
    - align init defaults for Viacoin user, config, pid, and datadir paths
    - keep shipped init filenames unchanged

--- src/init.cpp
+++ src/init.cpp
@@ -163,7 +163,7 @@ static const char* DEFAULT_ASMAP_FILENAME="ip_asn.map";
 /**
  * The PID file facilities.
  */
-static const char* BITCOIN_PID_FILENAME = "bitcoind.pid";
+static const char* BITCOIN_PID_FILENAME = "viacoind.pid";
 /**
  * True if this process has created a PID file.
  * Used to determine whether we should remove the PID file on shutdown.
